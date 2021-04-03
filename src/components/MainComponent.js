94ce2df7a2b954476265582d0af8c20683581a01
Touhedur Rahman <mtrshuvo@ymail.com>
2021-04-03 22:32:31 +0600

    setup-tree

---
+++ src/components/MainComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './header/Header.js'
+import Body from './body/Body.js'
+import Footer from './footer/Footer.js'
+
+const MainComponent = ()=>{
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+
+        </div>
+    )
+}
+
+export default MainComponent;
